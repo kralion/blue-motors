b5e115aaa8cd8e0bf65f701bc71be1dba9d09342
Brayan <joan300501@gmail.com>
2024-07-04 12:01:20 -0500

    Issue finished @kralion

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors : {
+				'red-dark': '#DF1D01',
+				'light-bg': "#202B31"
+			},
+		},
 	},
 	plugins: [],
 }
