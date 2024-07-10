57b47ed00dd7ee29165af3918d67f8dd591000ea
Brayan <joan300501@gmail.com>
2024-07-10 08:05:14 -0500

    fetch products

--- src/components/MenuTab.jsx
+++ src/components/MenuTab.jsx
@@ -1,122 +1,201 @@
+import ImagMenu from "./../assets/imagmenu.png";
 
-import ImagMenu from './../assets/imagmenu.png';
+import chaqueta from "../assets/chaqueta.png";
+import casco from "../assets/casco.png";
+import guantes from "../assets/guantes.png";
+import tubo from "../assets/tubo.png";
+import CardMenu from "./CardMenu";
 
-import chaqueta from '../assets/chaqueta.png'
-import casco from '../assets/casco.png'
-import guantes from '../assets/guantes.png'
-import tubo from '../assets/tubo.png'
-import CardMenu from './CardMenu';
-
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 
 export default function MenuTab() {
-    return (
-        <>
-            <TabGroup className='bg-white text-gray-500 flex '>
-                <TabList className='flex flex-col border-r-[1px] border-gray-200 *:py-[15px] *:pl-[20px] *:pr-[100px]'>
-                    <Tab
-                        className='data-[selected]:text-red-dark data-[selected]:font-medium
+  return (
+    <>
+      <TabGroup className="bg-white text-gray-500 flex ">
+        <TabList className="flex flex-col border-r-[1px] border-gray-200 *:py-[15px] *:pl-[20px] *:pr-[100px]">
+          <Tab
+            className="data-[selected]:text-red-dark data-[selected]:font-medium
                     focus:outline-none
-                    '
-                        key={"uno"}>
-                        FEATURED PRODUCTS
-                    </Tab>
-                    <Tab className='data-[selected]:text-red-dark data-[selected]:font-medium
+                    "
+            key={"uno"}
+          >
+            FEATURED PRODUCTS
+          </Tab>
+          <Tab
+            className="data-[selected]:text-red-dark data-[selected]:font-medium
                     focus:outline-none
-                    ' key={"dos"}>
-                        SALE OFF PRODUCTS
-                    </Tab>
-                    <div
-                        className='data-[selected]:text-red-dark data-[selected]:font-medium
+                    "
+            key={"dos"}
+          >
+            SALE OFF PRODUCTS
+          </Tab>
+          <div
+            className="data-[selected]:text-red-dark data-[selected]:font-medium
                     focus:outline-none hover:text-red-dark
-                    '
-                        key={"tres"}>
-                        <a href='/shop '>SHOP</a>
-                    </div>
-                </TabList>
-                <TabPanels className='p-[20px]'>
+                    "
+            key={"tres"}
+          >
+            <a href="/shop ">SHOP</a>
+          </div>
+        </TabList>
+        <TabPanels className="p-[20px]">
+          <TabPanel transition className="p-[15px]" key={""}>
+            <div className="flex ">
+              <div className="flex flex-col">
+                <div className="my-[20px]">
+                  <p className="font-medium mb-[10px]">FEATURE PRODUCTS</p>
+                  <div className="h-[2px] w-[20%] rounded-xl bg-red-dark"></div>
+                </div>
+                <CardMenu
+                  details="Camp Kids Boots"
+                  price="580.00"
+                  image={tubo}
+                  stars={3}
+                />
+                <CardMenu
+                  details="Box Plain Mat"
+                  price="154.00"
+                  image={casco}
+                  stars={4}
+                />
+                <CardMenu
+                  details="Abdes Texxtile Jacket"
+                  price="402.00"
+                  image={chaqueta}
+                  stars={5}
+                />
+              </div>
 
-                    <TabPanel
-                        transition
-                        className='p-[15px]'
-                        key={""}>
-                        <div className='flex '>
-                            <div className='flex flex-col'>
-                                <div className='my-[20px]'>
-                                    <p className="font-medium mb-[10px]">FEATURE PRODUCTS</p>
-                                    <div className='h-[2px] w-[20%] rounded-xl bg-red-dark'></div>
-                                </div>
-                                <CardMenu details="Camp Kids Boots" price="580.00" image={tubo} stars={3} />
-                                <CardMenu details="Box Plain Mat" price="154.00" image={casco} stars={4} />
-                                <CardMenu details="Abdes Texxtile Jacket" price="402.00" image={chaqueta} stars={5} />
+              <div>
+                <div className="my-[20px]">
+                  <p className="font-medium mb-[10px]">TOP RATED PRODUCTS</p>
+                  <div className="h-[2px] w-[20%] rounded-xl bg-red-dark"></div>
+                </div>
+                <CardMenu
+                  details="Camp Kids Boots"
+                  price="580.00"
+                  image={tubo}
+                  stars={3}
+                />
+                <CardMenu
+                  details="Box Plain Mat"
+                  price="154.00"
+                  image={casco}
+                  stars={4}
+                />
+                <CardMenu
+                  details="Abdes Texxtile Jacket"
+                  price="402.00"
+                  image={chaqueta}
+                  stars={5}
+                />
+              </div>
+              <div>
+                <div className="my-[20px]">
+                  <p className="font-medium mb-[10px]">SALE PRODUCTS</p>
+                  <div className="h-[2px] w-[20%] rounded-xl bg-red-dark"></div>
+                </div>
+                <CardMenu
+                  details="Camp Kids Boots"
+                  price="580.00"
+                  image={tubo}
+                  stars={3}
+                />
+                <CardMenu
+                  details="Box Plain Mat"
+                  price="154.00"
+                  image={casco}
+                  stars={4}
+                />
+                <CardMenu
+                  details="Abdes Texxtile Jacket"
+                  price="402.00"
+                  image={chaqueta}
+                  stars={5}
+                />
+              </div>
+            </div>
+          </TabPanel>
 
-                            </div>
+          <TabPanel key={""} className="p-[15px]">
+            <div className="flex ">
+              <div className="flex flex-col">
+                <div className="my-[20px]">
+                  <p className="font-medium mb-[10px]">PRODUCTS</p>
+                  <div className="h-[2px] w-[20%] rounded-xl bg-red-dark"></div>
+                </div>
+                <CardMenu
+                  details="Camp Kids Boots"
+                  price="580.00"
+                  image={tubo}
+                  stars={3}
+                />
+                <CardMenu
+                  details="Box Plain Mat"
+                  price="154.00"
+                  image={casco}
+                  stars={4}
+                />
+                <CardMenu
+                  details="Abdes Texxtile Jacket"
+                  price="402.00"
+                  image={chaqueta}
+                  stars={5}
+                />
+              </div>
 
-                            <div>
-                                <div className='my-[20px]'>
-                                    <p className="font-medium mb-[10px]">TOP RATED PRODUCTS</p>
-                                    <div className='h-[2px] w-[20%] rounded-xl bg-red-dark'></div>
-                                </div>
-                                <CardMenu details="Camp Kids Boots" price="580.00" image={tubo} stars={3} />
-                                <CardMenu details="Box Plain Mat" price="154.00" image={casco} stars={4} />
-                                <CardMenu details="Abdes Texxtile Jacket" price="402.00" image={chaqueta} stars={5} />
-
-                            </div>
-                            <div>
-                                <div className='my-[20px]'>
-                                    <p className="font-medium mb-[10px]">SALE PRODUCTS</p>
-                                    <div className='h-[2px] w-[20%] rounded-xl bg-red-dark'></div>
-                                </div>
-                                <CardMenu details="Camp Kids Boots" price="580.00" image={tubo} stars={3} />
-                                <CardMenu details="Box Plain Mat" price="154.00" image={casco} stars={4} />
-                                <CardMenu details="Abdes Texxtile Jacket" price="402.00" image={chaqueta} stars={5} />
-
-                            </div>
-
-                        </div>
-                    </TabPanel>
-
-                    <TabPanel key={""} className='p-[15px]'>
-                        <div className='flex '>
-                            <div className='flex flex-col'>
-                                <div className='my-[20px]'>
-                                    <p className="font-medium mb-[10px]">PRODUCTS</p>
-                                    <div className='h-[2px] w-[20%] rounded-xl bg-red-dark'></div>
-                                </div>
-                                <CardMenu details="Camp Kids Boots" price="580.00" image={tubo} stars={3} />
-                                <CardMenu details="Box Plain Mat" price="154.00" image={casco} stars={4} />
-                                <CardMenu details="Abdes Texxtile Jacket" price="402.00" image={chaqueta} stars={5} />
-
-                            </div>
-
-                            <div>
-                                <div className='my-[20px]'>
-                                    <p className="font-medium mb-[10px]">SALE OFF</p>
-                                    <div className='h-[2px] w-[20%] rounded-xl bg-red-dark'></div>
-                                </div>
-                                <CardMenu details="Camp Kids Boots" price="580.00" image={tubo} stars={3} />
-                                <CardMenu details="Box Plain Mat" price="154.00" image={casco} stars={4} />
-                                <CardMenu details="Abdes Texxtile Jacket" price="402.00" image={chaqueta} stars={5} />
-
-                            </div>
-                            <div>
-                                <div className='my-[20px]'>
-                                    <p className="font-medium mb-[10px]">SALE TAGS</p>
-                                    <div className='h-[2px] w-[20%] rounded-xl bg-red-dark'></div>
-                                </div>
-                                <CardMenu details="Camp Kids Boots" price="580.00" image={tubo} stars={3} />
-                                <CardMenu details="Box Plain Mat" price="154.00" image={casco} stars={4} />
-                                <CardMenu details="Abdes Texxtile Jacket" price="402.00" image={chaqueta} stars={5} />
-
-                            </div>
-
-                        </div>
-                    </TabPanel>
-
-
-                </TabPanels>
-            </TabGroup>
-        </>
-    )
+              <div>
+                <div className="my-[20px]">
+                  <p className="font-medium mb-[10px]">SALE OFF</p>
+                  <div className="h-[2px] w-[20%] rounded-xl bg-red-dark"></div>
+                </div>
+                <CardMenu
+                  details="Camp Kids Boots"
+                  price="580.00"
+                  image={tubo}
+                  stars={3}
+                />
+                <CardMenu
+                  details="Box Plain Mat"
+                  price="154.00"
+                  image={casco}
+                  stars={4}
+                />
+                <CardMenu
+                  details="Abdes Texxtile Jacket"
+                  price="402.00"
+                  image={chaqueta}
+                  stars={5}
+                />
+              </div>
+              <div>
+                <div className="my-[20px]">
+                  <p className="font-medium mb-[10px]">SALE TAGS</p>
+                  <div className="h-[2px] w-[20%] rounded-xl bg-red-dark"></div>
+                </div>
+                <CardMenu
+                  details="Camp Kids Boots"
+                  price="580.00"
+                  image={tubo}
+                  stars={3}
+                />
+                <CardMenu
+                  details="Box Plain Mat"
+                  price="154.00"
+                  image={casco}
+                  stars={4}
+                />
+                <CardMenu
+                  details="Abdes Texxtile Jacket"
+                  price="402.00"
+                  image={chaqueta}
+                  stars={5}
+                />
+              </div>
+            </div>
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
+    </>
+  );
 }
-
