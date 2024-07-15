e7e7e28e3537594b1a5b5da45b1d1f08f33399e4
Brayan <joan300501@gmail.com>
2024-07-15 17:00:59 -0500

    adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+ import netlify from '@astrojs/netlify';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+  adapter: netlify(),
+  image: {
+      domains: ['bluemotorsec.com','azwecdnepstoragewebsiteuploads.azureedge.net', 'img.freepik.com'],
+    },
   integrations: [tailwind(), react()],
 });
